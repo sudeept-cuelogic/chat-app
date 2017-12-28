1905fceb0d0b528c6fb211f98434ee0ed01a0de4
sudeept-cuelogic <sudeep.tarlekar@cuelogic.com>
2017-12-28 19:31:03 +0530

    Added callbacks on socket.

--- server.js
+++ server.js
@@ -1,32 +1,25 @@
 var dot = require('dotenv').config();
-var http = require('http');
+var server = require('http').createServer(handler);
 var io = require('socket.io')(server);
 var fs = require('fs');
 
-var server = http.createServer((req, res) => {
+function handler (req, res) {
   fs.readFile(__dirname + '/index.html', (err, data) => {
     if (err) {
       res.writeHead(500);
       return res.end(`Internal server error ${err}`);
     }
-
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.writeHead(200);
     res.end(data);
-  })
-})
+  });
+};
 
 server.listen(process.env.SERVER_PORT);
 
-var handler = (req, res) => {
-  fs.readFile(__dirname + 'index.html', (err, data) => {
-    if (err) {
-      res.writeHead(500);
-      res.end('Something went wrong while starting a server');
-    } else {
-      res.writeHead(200);
-      re.end(data);
-    }
+io.on('connection', (socket) => {
+  socket.on('send_msg', function(msg) {
+    console.log(msg);
   })
-}
-console.log('Server started at localhost:3000');
+});
 
+console.log('Server started at localhost:3000');
