866b3354d8e48ce44d8fb008eb5fafe175231791
sudeept-cuelogic <sudeep.tarlekar@cuelogic.com>
2017-12-28 18:24:46 +0530

    added socker io.

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
-var http = require('http');
 var dot = require('dotenv').config();
+var http = require('http');
+var io = require('socket.io')(server);
+var fs = require('fs');
 
 var server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end();
-});
+  fs.readFile(__dirname + '/index.html', (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      return res.end(`Internal server error ${err}`);
+    }
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  })
+})
 
 server.listen(process.env.SERVER_PORT);
 
+var handler = (req, res) => {
+  fs.readFile(__dirname + 'index.html', (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      res.end('Something went wrong while starting a server');
+    } else {
+      res.writeHead(200);
+      re.end(data);
+    }
+  })
+}
 console.log('Server started at localhost:3000');
 
