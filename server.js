1ce7baaba17b3903a162c1e12422731226685dd1
sudeept-cuelogic <sudeep.tarlekar@cuelogic.com>
2017-12-29 19:04:15 +0530

    Added form for employee.

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ var app = require('express')();
 var http = require('http').Server(app)
 var io = require('socket.io')(http);
 
+app.set('view engine', 'pug');
+
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/employee', function(req, res) {
+  console.log('serving employee')
+  res.render(__dirname + '/employee.pug');
 });
 
 http.listen(process.env.SERVER_PORT, function() {
